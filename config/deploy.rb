772379d8b894d76a53e21d26aac3916020450ee8
Hugo Tunius <hugo.tunius@gmail.com>
2014-01-02 18:06:21 +0100

    Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,14 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'hugotunius.se'
+set :repository,  '_site'
+set :scm, :git
+set :repo_url, 'git@hugotunius.se:hugotunius.se.git'
+set :deploy_via, :remote_cache
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :user, "deployer"
+set :use_sudo, false
+set :deploy_to, "/var/www/hugotunius"
+set :ssh_options, { user: "deployer", forward_agent: true, auth_methods: %w(publickey password) }
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
 
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,10 @@
 set :stage, :production
+set :branch, :master
+set :user, "deployer"
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :all, "hugotunius.se"
+
+
 
 # Extended Server Syntax
 # ======================
@@ -15,8 +12,6 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
