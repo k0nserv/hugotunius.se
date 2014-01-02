28a09bb783ca06ff5409f256be2f1f1078ed8f80
Hugo Tunius <hugo.tunius@gmail.com>
2014-01-02 18:39:19 +0100

    Add bundle and cleanup commands to capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,22 @@ set :repo_url, 'git@hugotunius.se:hugotunius.se.git'
 set :deploy_via, :remote_cache
 
 set :user, "deployer"
+set :group, "www-data"
 set :use_sudo, false
 set :deploy_to, "/var/www/hugotunius"
 set :ssh_options, { user: "deployer", forward_agent: true, auth_methods: %w(publickey password) }
 
-namespace :deploy do
+after 'deploy:finished', 'deploy:update_jekyll'
 
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    %x(rm -rf _site/* && bundle && jekyll build))
+    %x(shopt -s extglob dotglob && rm -rf !(_site))
+  end
 end
