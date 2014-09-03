aa87005bb5d8d0fd2b1aecf1c36e2374f1a18536
Hugo Tunius <hugo.tunius@gmail.com>
2014-09-03 21:15:47 +0200

    Revert "Specify bundle command"
    This reverts commit 5b73272eb8514fdb043427722589757a45ab9884.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby jekyll}
 set :rbenv_roles, :all # default value
-set :bundle_cmd { "#{release_path}/bin/bundle" }
 
 set :default_env, {
   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
